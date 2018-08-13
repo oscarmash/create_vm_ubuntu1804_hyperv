9485b81df6dbd6ff8135e3c1169a69f70acd553f
root <root@ansible.ilba.cat>
2018-08-13 15:05:53 +0200

    first commit

---
+++ shared-files/create_vm.ps1
@@ -0,0 +1,19 @@
+﻿$VM = $args[0]
+$CPU = $args[1]
+$RAM = $args[2]
+
+new-item "D:\$VM" -itemtype directory
+new-item "D:\$VM\Virtual Hard Disks" -itemtype directory
+$Path = "D:\" + $VM
+$Source = 'E:\ExportVM\template_Ubuntu18.04LTS\Virtual Machines\0D9EBA7D-A359-46CB-A2BA-826C9DB10787.vmcx'
+$Import = Import-VM -Path $Source -Copy -GenerateNewId -VirtualMachinePath $Path -VhdDestinationPath "D:\$VM\Virtual Hard Disks" -SnapshotFilePath $Path
+Foreach ($Import in $Import)
+{
+    $oldName = $Import.Name
+    Rename-VM $oldName -NewName $VM
+}
+
+SET-VMProcessor -VMname "$VM" -count $CPU
+Set-VMMemory $VM -StartupBytes $RAM
+
+Start-VM -Name $VM
