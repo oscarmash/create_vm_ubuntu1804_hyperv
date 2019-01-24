5580b4ee823c4566a24355228d3f8284d23bdb2d
Oscar Mas <oscarmash@gmail.com>
2019-01-24 21:46:20 +0100

    Update create_vm.ps1

--- shared-files/create_vm.ps1
+++ shared-files/create_vm.ps1
@@ -5,7 +5,7 @@ $RAM = $args[2]
 new-item "D:\$VM" -itemtype directory
 new-item "D:\$VM\Virtual Hard Disks" -itemtype directory
 $Path = "D:\" + $VM
-$Source = 'E:\ExportVM\template_Ubuntu18.04LTS\Virtual Machines\0D9EBA7D-A359-46CB-A2BA-826C9DB10787.vmcx'
+$Source = 'E:\ExportVM\template_Ubuntu18.04LTS\Virtual Machines\E60FC1F8-6C29-4DFD-8C28-DC37847FD6FF.vmcx'
 $Import = Import-VM -Path $Source -Copy -GenerateNewId -VirtualMachinePath $Path -VhdDestinationPath "D:\$VM\Virtual Hard Disks" -SnapshotFilePath $Path
 Foreach ($Import in $Import)
 {
